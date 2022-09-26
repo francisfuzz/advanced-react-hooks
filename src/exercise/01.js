7bc3ca80387988e7552ec7ed54faf41e6f646923
Francis <15894826+francisfuzz@users.noreply.github.com>
2022-09-26 22:31:35 +0000

    feat: add ex 01 ec 03

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,7 +3,18 @@
 
 import * as React from 'react'
 
+// https://stackoverflow.com/a/7356528
+function isFunction(functionToCheck) {
+  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+ }
+
 function countReducer(state, action) {
+  // If the action is a function, return the evaluated expression of the function
+  // passing in the current state.
+  if (isFunction(action)) {
+    return action(state)
+  }
+
   return {
     ...state,
     ...action
@@ -18,7 +29,13 @@ function Counter({initialCount = 0, step = 1}) {
   const { count } = state
 
   const increment = () => setState({count: count + step})
-  return <button onClick={increment}>{count}</button>
+  const decrement = () => setState(currentState => ({count: currentState.count - step}))
+  return (
+    <>
+      <button onClick={increment}>{count}</button>
+      <button onClick={decrement}>{count}</button>
+    </>
+  )
 }
 
 function App() {
